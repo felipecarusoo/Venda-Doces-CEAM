1c134719e263d0426bbf2100801f8131a76ec055
Wonder <101845224+felipecarusoo@users.noreply.github.com>
2023-10-09 00:41:09 -0300

    Add files via upload

--- assets/js/avaliacao.js
+++ assets/js/avaliacao.js
@@ -1,35 +1,70 @@
-var imgBrigadeiro = document.getElementById('imgBrigadeiro');
-var imgCupcake = document.getElementById('imgCupcake');
-var imgBrownie = document.getElementById('imgBrownie');
-var mensssagem = document.getElementById('mensssagem');
-var produto = document.getElementById('produto')
+document.addEventListener('DOMContentLoaded', function () {
+    const imgBrigadeiro = document.getElementById('imgBrigadeiro');
+    const imgCupcake = document.getElementById('imgCupcake');
+    const imgBrownie = document.getElementById('imgBrownie');
+    const produto = document.getElementById('produto');
+    const ratingButtons = document.querySelectorAll('.rating-button');
+    const hiddenAvaliacao = document.getElementById('avaliacao');
+    const message = document.getElementById('message');
 
-imgBrigadeiro.addEventListener('click', function () {
-    if (!produto.value.includes('Brigadeiro ')) {
-        produto.value += 'Brigadeiro \n';
-    }
-});
+    imgBrigadeiro.addEventListener('click', function () {
+        if (!produto.value.includes('Brigadeiro')) {
+            if (produto.value.length > 0) {
+                produto.value += ', ';
+            }
+            produto.value += 'Brigadeiro';
+        }
+    });
 
-imgCupcake.addEventListener('click', function () {
-    if (!produto.value.includes('Cupcake ')) {
-        produto.value += 'Cupcake \n';
-    }
-});
+    imgCupcake.addEventListener('click', function () {
+        if (!produto.value.includes('Cupcake')) {
+            if (produto.value.length > 0) {
+                produto.value += ', ';
+            }
+            produto.value += 'Cupcake';
+        }
+    });
 
-imgBrownie.addEventListener('click', function () {
-    if (!produto.value.includes('Brownie ')) {
-        produto.value += 'Brownie \n';
-    }
-});
+    imgBrownie.addEventListener('click', function () {
+        if (!produto.value.includes('Brownie')) {
+            if (produto.value.length > 0) {
+                produto.value += ', ';
+            }
+            produto.value += 'Brownie';
+        }
+    });
 
+    ratingButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const value = button.value;
+            hiddenAvaliacao.value = value;
+            ratingButtons.forEach(btn => {
+                if (btn.value <= value) {
+                    btn.classList.add('selected');
+                } else {
+                    btn.classList.remove('selected');
+                }
+            });
 
-const ratingButtons = document.querySelectorAll('.rating-button');
-const hiddenAvaliacao = document.getElementById('avaliacao');
-
-ratingButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        hiddenAvaliacao.value = button.value;
-        ratingButtons.forEach(btn => btn.classList.remove('selected'));
-        button.classList.add('selected');
+            switch (value) {
+                case '1':
+                    message.value = 'Muito ruim';
+                    break;
+                case '2':
+                    message.value = 'Ruim';
+                    break;
+                case '3':
+                    message.value = 'Regular';
+                    break;
+                case '4':
+                    message.value = 'Bom';
+                    break;
+                case '5':
+                    message.value = 'Excelente';
+                    break;
+                default:
+                    message.value = '';
+            }
+        });
     });
 });
